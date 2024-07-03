1ccb6984a2bfd6d7cb3afaaa1d58fd3d020f1b88
Binod Chaudhary <thebinod7@gmail.com>
2024-07-03 06:40:40 +0545

    cleanup

--- array/second_largest.js
+++ array/second_largest.js
@@ -1,19 +1,19 @@
 const inputArr = [3, 23, 44, 3, 34, 23, 44, 22];
 
-function findABC(arr) {
-	let largest = -1;
-	let secondLargest = -1;
+function findSecondLargest(arr) {
+  let largest = -1;
+  let secondLargest = -1;
 
-	for (let i = 0; i < arr.length; i++) {
-		if (arr[i] > largest) {
-			secondLargest = largest;
-			largest = arr[i];
-		} else if (arr[i] !== largest && arr[i] > secondLargest) {
-			secondLargest = arr[i];
-		}
-	}
-	return secondLargest;
+  for (let i = 0; i < arr.length; i++) {
+    if (arr[i] > largest) {
+      secondLargest = largest;
+      largest = arr[i];
+    } else if (arr[i] !== largest && arr[i] > secondLargest) {
+      secondLargest = arr[i];
+    }
+  }
+  return secondLargest;
 }
 
-const result = findABC(inputArr); // Time complexity: O(n)
+const result = findSecondLargest(inputArr); // Time complexity: O(n)
 console.log("Second largest number is: ", result);
