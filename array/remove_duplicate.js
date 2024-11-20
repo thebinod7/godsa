a8cf3a23439be4d46f18620a6a7ca1d0a4dbd78e
Binod Chaudhary <thebinod7@gmail.com>
2024-11-20 09:00:50 +0545

    realgin

--- array/remove_duplicate.js
+++ array/remove_duplicate.js
@@ -1,31 +1,34 @@
-const sample = [1, 1, 1, 2, 3, 4, 5, 5];
+const SAMPLE = [1, 1, 1, 2, 3, 4, 5, 5];
 
-// Keep it in same order
-// Dont' use extra space
-
+//
 function removeDups(inputArr) {
-	for (let i = 0; i < inputArr.length - 1; i++) {
-		if (inputArr[i] === inputArr[i + 1]) {
-			inputArr.splice(i, 1); // Time complexity: O(1)
-			i--;
-		}
-	}
-	return inputArr.length;
+  for (let i = 0; i < inputArr.length - 1; i++) {
+    if (inputArr[i] === inputArr[i + 1]) {
+      inputArr.splice(i, 1); // Time complexity: O(1)
+      i--;
+    }
+  }
+  console.log("Non Duplicate Array: ", inputArr);
+  return inputArr.length;
 }
 
-const result = removeDups(sample); // Time complexity: O(n) + O(1) = O(n)
-console.log("ResultArray: ", result); //
+const result = removeDups(SAMPLE); // Time complexity: O(n) + O(1) = O(n)
+console.log("Non duplicate count: ", result); //
 
 // ===============================================
 function removeDupsWithoutArraMethod(nums) {
-	let i = 0;
-	for (j = 1; j < nums.length; j++) {
-		if (nums[i] !== nums[j]) {
-			i++;
-			nums[i] = nums[j];
-		}
-	}
-	return i + 1;
+  let i = 0;
+  for (j = 1; j < nums.length; j++) {
+    if (nums[i] !== nums[j]) {
+      i++;
+      nums[i] = nums[j];
+    }
+  }
+  return i + 1;
 }
 
-console.log("ResultNonArray: ", removeDupsWithoutArraMethod(sample)); // Time complexity: O(n)
+// Time complexity: O(n)
+console.log(
+  "Non duplicate count without inbuilt method: ",
+  removeDupsWithoutArraMethod(SAMPLE)
+);
--- array/rotate.js
+++ array/rotate.js
@@ -1,30 +1,33 @@
+const SAMPLE = [1, 2, 3, 4, 5, 6];
+const ROTATION_KEY = 2;
+
 function rotateByK(arrInput, k) {
-	const size = arrInput.length;
-	if (k > size) {
-		k = k % size;
-	}
-	reverse(arrInput, 0, size - 1);
-	console.log("1st reverse: ", arrInput); // Time complexity: O(n)
-	reverse(arrInput, 0, k - 1);
-	console.log("2nd reverse: ", arrInput); // Time complexity: O(k)
-	reverse(arrInput, k, size - 1);
-	console.log("3rd reverse: ", arrInput); // Time complexity: O(n-k)
-	return arrInput;
+  const size = arrInput.length;
+  if (k > size) k = k % size;
+
+  reverse(arrInput, 0, size - 1);
+  console.log("Reverse entire array: ", arrInput); // Time complexity: O(n)
+  reverse(arrInput, 0, k - 1);
+  console.log("Reverse first part : ", arrInput); // Time complexity: O(k)
+  reverse(arrInput, k, size - 1);
+  console.log("Reverse second reverse: ", arrInput); // Time complexity: O(n-k)
+  return arrInput;
 }
 
 // Similar to sort operation
-function reverse(nums, left, right) {
-	while (left < right) {
-		let temp = nums[left];
-		nums[left] = nums[right];
-		nums[right] = temp;
-		left++;
-		right--;
-	}
+function reverse(nums, start, end) {
+  while (start < end) {
+    let temp = nums[start];
+    nums[start] = nums[end];
+    nums[end] = temp;
+    start++;
+    end--;
+  }
 }
 
-const result = rotateByK([1, 2, 3, 4, 5, 6], 3);
-console.log("Rotated array is: ", result); // Time complexity: O(n
+const result = rotateByK(SAMPLE, ROTATION_KEY);
+console.log("Ratation key: ", ROTATION_KEY);
+console.log("Rotated array: ", result); // Time complexity: O(n
 
 // ==========OPERATIONS============
 // [6,5,4,3,2,1] [0-size-1]
